76c29c29c434b2d041a121d40768e46b1d90e34a
Paul Mendoza <pauljoshuam@hotmail.com>
2016-07-06 11:07:42 -0700

    init app. run with webpack -w

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+      return (
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+            <h4>Welcome to Get Rec'd</h4>
+          </div>
+        </nav>
+    </div>
+    )
+}
+
+export default Main
